f52bae4b466f98254b05ecc31233aacac3c429ce
Asrat77 <112464138+Asrat77@users.noreply.github.com>
2023-06-01 09:02:23 +0300

    Update tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,24 +6,56 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    if task.save
-      render json: {data: @task, status: 200,  message: "success"}
+    if @task.save
+      render json: {data: @task, status: 200,  message: "successfully created"}
     else
       render json: {data: @task.errors, status: 400,  message: "error"}
 
   end
 
-  def edit
-  end
+  rescue ActiveRecordRecordNotFound => e
+    render json: {data: e.message, status: 404, message: "record not found"}
+  rescue ActiveRecord::RecordInvalid => e
+    render json: {data: e.message, status: 422, message: "unprocessable entity"}
+  rescue StandardError => e
+    render json: {data: e.message, status: 500, message: "internal server error"}
+end
 
+  
   def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      render json: {data: @task, status: 200,  message: "successfully updated"}
+    else
+      render json: {data: @task.errors, status: 400,  message: "error"}
+    end
+  rescue ActiveRecordRecordNotFound => e
+    render json: {data: e.message, status: 404, message: "record not found"}
+  rescue ActiveRecord::RecordInvalid => e
+    render json: {data: e.message, status: 422, message: "unprocessable entity"}
+  rescue StandardError => e
+    render json: {data: e.message, status: 500, message: "internal server error"}
+
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    if @task.destroy
+      render json: {data: @task, status: 200,  message: "successfully deleted"}
+    else
+      render json: {data: @task.errors, status: 400,  message: "error"}
+    end
+    
+  rescue ActiveRecordRecordNotFound => e
+    render json: {data: e.message, status: 404, message: "record not found"}
+  rescue ActiveRecord::RecordInvalid => e
+    render json: {data: e.message, status: 422, message: "unprocessable entity"}
+  rescue StandardError => e
+    render json: {data: e.message, status: 500, message: "internal server error"}
   end
 
   private
-        def task_params
-            params.require(:tasks).permit(:title, :description, :status, :user_id)
-        end
+  def task_params
+      params.require(:tasks).permit(:title, :description, :status, :user_id)
+  end
 end
